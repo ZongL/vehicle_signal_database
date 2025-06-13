db6d483c703836a2db0a0d3e28f686d3efb195cc
zongliang <zongliang88888@163.com>
2025-06-13 20:00:12 +0800

    remove Invoices&Customers from left navigation

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -17,12 +17,8 @@ import clsx from 'clsx';
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  // { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon},
+  // { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
   { name: 'ECUs', href:'/dashboard/ecus', icon: CpuChipIcon},
   { name: 'Messages', href:'/dashboard/messages', icon: EnvelopeIcon},
   { name: 'Signals', href:'/dashboard/signals', icon: DocumentChartBarIcon},
